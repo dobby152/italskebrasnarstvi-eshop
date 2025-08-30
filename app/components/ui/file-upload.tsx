612767fc3c7ea8bd0024ec0dd41e67a53459665e
dobby152 <fiala@askela.cz>
2025-08-30 16:05:56 +0200

    🔧 Fix all @/lib/utils imports in UI components - use relative paths

--- app/components/ui/file-upload.tsx
+++ app/components/ui/file-upload.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 import { Upload, X, Image as ImageIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
--- app/components/ui/product-tag.tsx
+++ app/components/ui/product-tag.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Badge } from './badge'
-import { cn } from '@/lib/utils'
+import { cn } from '../../lib/utils'
 
 export interface Tag {
   id: number
